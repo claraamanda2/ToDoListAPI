08a35aa4b0b8493ac32882a4a32888533b6bb258
claraamanda2 <clara.froes@aluno.senai.br>
2026-04-02 15:10:11 -0300

    feat: Implementamos o service e o controller do nosso usuário

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Aula_01.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
@@ -24,6 +25,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<UsuarioService>();
 
 // Customização da resposta de validação (Data Annotations)
 builder.Services.Configure<ApiBehaviorOptions>(options =>
